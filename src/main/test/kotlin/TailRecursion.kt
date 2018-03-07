fccd28880bddf6d682d0debfec071006c94f5088
Jan Galinski <jan.galinski@holisticon.de>
2018-03-07 16:16:43 +0100

    use when in filter

--- src/main/test/kotlin/TailRecursion.kt
+++ src/main/test/kotlin/TailRecursion.kt
@@ -2,26 +2,18 @@ package net.janhoo.playground
 
 
 fun main(args: Array<String>) {
-  val f: (i:Int) -> Boolean = { it % 2 == 0 }
-  println(filter(l = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9), f = f))
+  val f: (i: Int) -> Boolean = { it % 2 == 0 }
+  println(filter(list = listOf(1..100).flatten(), predicate = f))
 
-  println(filter(l= emptyList(), f=f))
+  println(filter(list = emptyList(), predicate = f))
 }
 
-fun <T> List<T>.tail() = drop(1)
+fun <T> List<T>.tail() = subList(1, size)
 fun <T> List<T>.head() = first()
 
-tailrec fun <T> filter(l: List<T>, res: List<T> = emptyList(), f: (T) -> Boolean): List<T> {
-  if (l.isEmpty()) {
-    return res
-  } else {
-    return filter(
-      l.tail(),
-      if (f(l.head())) {
-        res + listOf(l.head())
-      } else {
-        res
-      },
-      f)
-  }
+tailrec fun <T> filter(list: List<T>, result: List<T> = emptyList(), predicate: (T) -> Boolean): List<T> = when {
+  list.isEmpty() -> result // no more elements -> return result
+  predicate(list.head()) -> filter(list.tail(), result + list.head(), predicate) // first element matches -> add to result, recurse with tail
+  else -> filter(list.tail(), result, predicate) // first element does not match -> recurse with tail
 }
+
